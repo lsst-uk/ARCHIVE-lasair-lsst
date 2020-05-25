c7116dbbe4624fe19d90f0f23ec3f14f3443f1cf
Roy Williams <roy@roe.ac.uk>
2020-05-25 09:11:10 +0000

    update

--- lasair-db/archive_in.py
+++ lasair-db/archive_in.py
@@ -1,14 +1,15 @@
 import os, sys
+import settings
 file = sys.argv[1]
 print(file)
 
 sql  = "LOAD DATA LOCAL INFILE '/home/ubuntu/scratch/%s' " % file
-sql += "REPLACE INTO TABLE candidates FIELDS TERMINATED BY ',' "
+sql += "REPLACE INTO TABLE objects FIELDS TERMINATED BY ',' "
 sql += "ENCLOSED BY '\"' LINES TERMINATED BY '\n'"
 
 f = open('tmp.sql', 'w')
 f.write(sql)
 f.close()
 
-cmd =  "mysql --user=ztf --database=ztf --password=123password < tmp.sql"
+cmd =  "mysql --user=ztf --database=ztf --password=%s < tmp.sql" % settings.DB_PASSWORD
 os.system(cmd)
