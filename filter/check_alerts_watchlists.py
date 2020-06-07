819bb5a374e78680c816907c81c98beb38b48b77
Roy Williams <roy@roe.ac.uk>
2020-06-07 11:25:08 +0000

    added watchlist code

---
+++ filter/check_alerts_watchlists.py
@@ -0,0 +1,160 @@
+"""
+check_alerts_watchlists.py
+This code checks a batch of alerts against the cached watchlist files,
+that can be used for very fast crossmatch against alerts. The files are kept in
+a directory named wl_<nn> where nn is the watchlist id from the database. Inside:
+     watchlist.csv
+     moc000.fits
+     moc001.fits
+     ...
+where the first has the columns
+    cone_id, ra, dec, radius, name
+where cone_id is the id of the cone in the database, at the given position and radius
+(in degrees), with the user-given name of the cone last. The "moc<nnn>.fits" files are
+"Multi-Order Coverage maps", https://cds-astro.github.io/mocpy/. The union of all the
+files is the same as the list of cones associated with the watchlist.
+"""
+import os
+import math
+import settings
+from mocpy import MOC
+import astropy.units as u
+
+def read_watchlist_cache_files():
+    """
+    This function reads all the files in the cache directories and keeps them in memory
+    in a list called "watchlistlist". Each watchlist is a dictionary:
+        cone_ids: the integer ids of the watchlist cones
+        ra, de: the lists of ra and dec posiitons of the cones, in degrees
+        radius: the list of radii of the cones about those points
+        names: the naems give tot he cones by the user
+    """
+    watchlistlist = []
+    for wl_dir in os.listdir(settings.WATCHLIST_MOCS):
+        # every directory in the cache should be of the form wl_<nn> 
+        # where nn is the watchlist id
+        try:     wl_id = int(wl_dir[3:])
+        except:  continue
+
+        # id of the watchlist
+        watchlist = {'wl_id':wl_id}
+
+        moclist = []
+        for file in os.listdir(settings.WATCHLIST_MOCS + wl_dir):
+            gfile = settings.WATCHLIST_MOCS + wl_dir + '/' + file
+
+            # read in the mocs
+            if file.startswith('moc'):
+                moclist.append(MOC.from_fits(gfile))
+
+            # read in the csv files of watchlist cones
+            if file.startswith('watchlist'):
+                cone_ids = []
+                ralist   = []
+                delist   = []
+                radius   = []
+                names    = []
+                for line in open(gfile).readlines():
+                    tok = line.split(',')
+                    cone_ids.append(int(tok[0]))
+                    ralist.append(float(tok[1]))
+                    delist.append(float(tok[2]))
+                    radius.append(float(tok[3]))
+                    names.append(tok[4].strip())
+                watchlist['cones'] = {
+                    'cone_ids':cone_ids, 
+                    'ra':ralist, 'de':delist, 
+                    'radius':radius, 
+                    'names':names
+                }
+            watchlist['moclist'] = moclist
+
+        watchlistlist.append(watchlist)
+    return watchlistlist
+
+def check_alerts_against_moc(alertlist, wl_id, moc, cones):
+    """ For a given moc, check the alerts in the batch 
+    """
+    # alert positions
+    alertralist = alertlist['ra']
+    alertdelist = alertlist['de']
+
+    # watchlist cones
+    watchralist = cones['ra']
+    watchdelist = cones['de']
+    watchradius = cones['radius']
+
+    # here is the crossmatch
+    result = moc.contains(alertralist*u.deg, alertdelist*u.deg)
+
+    hits = []
+    # go through the boolean vector, looking for hits
+    for ialert in range(len(alertralist)):
+        if(result[ialert]):
+            # when there is a hit, we need to know *which* cone contains the alert
+            ra = alertralist[ialert]
+            de = alertdelist[ialert]
+            for iw in range(len(watchralist)):
+                # don't forget the loxodrome
+                dra = (ra - watchralist[iw])*math.cos(de*math.pi/180)
+                dde = (de - watchdelist[iw])
+                # dra and dde are angular great circle distance
+                d = math.sqrt(dra*dra + dde*dde)
+                if d < watchradius[iw]:
+                    # got a real hit -- record the crossmatch
+                    hits.append({
+                        'ialert' :ialert,
+                        'wl_id'  :wl_id, 
+                        'cone_id':cones['cone_ids'][iw],
+                        'name'   :cones['names'][iw],
+                        'arcsec' : d*3600
+                    })
+
+    return hits
+
+def check_alerts_against_watchlist(alertlist, watchlist):
+    """ This function goes through all the watchlists looking for hits
+    """
+    moclist = watchlist['moclist']
+    cones   = watchlist['cones']
+    wl_id   = watchlist['wl_id']
+    hits = []
+    chk = settings.WATCHLIST_CHUNK
+    # larger watchlists are expressed by multiple mocs
+    for ichunk in range(len(moclist)):
+        coneschunk  = {
+            'cone_ids':cones['cone_ids'][ichunk*chk:(ichunk+1)*chk],
+            'ra'      :cones['ra']      [ichunk*chk:(ichunk+1)*chk],
+            'de'      :cones['de']      [ichunk*chk:(ichunk+1)*chk],
+            'radius'  :cones['radius']  [ichunk*chk:(ichunk+1)*chk],
+            'names'   :cones['names']   [ichunk*chk:(ichunk+1)*chk],
+        }
+        hits += check_alerts_against_moc(alertlist, wl_id, moclist[ichunk], coneschunk)
+    return hits
+
+def check_alerts_against_watchlists(alertlist, watchlistlist):
+    """ check the batch of alerts agains all the watchlists
+    """
+    hits = []
+    for watchlist in watchlistlist:
+        hits += check_alerts_against_watchlist(alertlist, watchlist)
+    return hits
+
+if __name__ == "__main__":
+    import random
+    Nwatch = 1000000
+    ralist = []
+    delist = []
+    for i in range(Nwatch):
+        ralist.append(360*random.random())
+        delist.append(-85 + 170*random.random())
+    alertlist = {"ra":ralist, "de":delist}
+    print('alertlist with %d points' % Nwatch)
+
+    # read in the cache files
+    watchlistlist = read_watchlist_cache_files()
+
+    # check the list against the watchlists
+    hits = check_alerts_against_watchlists(alertlist, watchlistlist)
+
+    for hit in hits: print(hit)
---
+++ filter/make_watchlist_files.py
@@ -0,0 +1,184 @@
+"""
+make_watchlist_files.py
+This code uses watchlist information in the database to build and cache files 
+that can be used for very fast crossmatch against alerts. The files are kept in
+a directory named wl_<nn> where nn is the watchlist id from the database. Inside:
+     watchlist.csv
+     moc000.fits
+     moc001.fits
+     ...
+where the first has the columns 
+    cone_id, ra, dec, radius, name
+where cone_id is the id of the cone in the database, at the given position and radius
+(in degrees), with the user-given name of the cone last. The "moc<nnn>.fits" files are 
+"Multi-Order Coverage maps", https://cds-astro.github.io/mocpy/. The union of all the 
+files is the same as the list of cones associated with the watchlist.
+"""
+from mocpy import MOC
+import astropy.units as u
+import mysql.connector
+import settings
+import math
+import time
+import os
+import stat
+
+def moc_watchlist(watchlist, max_depth):
+    """
+    Take a "watchlist" dictionary and builds a MOC at given max_depth, by approximating the
+    disk around a source as a hexagon.
+    """
+    s = 0.5*math.sqrt(3)
+    moc = None
+    ralist = watchlist['ra']      # ra list of the watchlist, degrees
+    delist = watchlist['de']      # dec list of the watchlist, degrees
+    radius = watchlist['radius']  # radius list in degrees
+    for i in range(len(ralist)):
+        ra = ralist[i]
+        de = delist[i]
+        q = radius[i]
+        r = q/math.cos(de*math.pi/180)
+
+        # make a hexagon
+        lon = [ra+q, ra+0.5*r, ra-0.5*r, ra-r, ra-0.5*r, ra+0.5*r] * u.deg
+        lat = [de,     de+s*q,  de+s*q,   de,   de-s*q,   de-s*q] * u.deg
+
+        # make a moc from the hexagon
+        newmoc = MOC.from_polygon(lon, lat, max_depth=max_depth)
+
+        # union with previous hexagons
+        if moc: moc = moc.union(newmoc)
+        else:   moc = newmoc
+    return moc
+
+def moc_watchlists(watchlist, max_depth):
+    """
+    mocs can be inefficient when they have a lot of points in them
+    so here we make several smaller mocs, each with a max number of cones
+    """
+    chk = settings.WATCHLIST_CHUNK   # max points per moc, perhaps 50,000
+
+    # split the number of points in the watchlist into chunks
+    nchunk = len(watchlist['ra'])//chk
+    if len(watchlist['ra']) % chk: 
+        nchunk += 1
+
+    # split up the watchlist and make a moc for each chunk
+    moclist = []
+    for ichunk in range(nchunk):
+        watchchunk  = {
+            'ra':     watchlist['ra']    [ichunk*chk:(ichunk+1)*chk],
+            'de':     watchlist['de']    [ichunk*chk:(ichunk+1)*chk],
+            'radius': watchlist['radius'][ichunk*chk:(ichunk+1)*chk]
+        }
+        moclist.append(moc_watchlist(watchchunk, max_depth))
+    # return a list of mocs
+    return moclist
+
+def connect_db():
+    msl = mysql.connector.connect(
+        user    =settings.DB_USER_REMOTE,
+        password=settings.DB_PASS_REMOTE,
+        host    =settings.DB_HOST_REMOTE,
+        database='ztf')
+    return msl
+
+def fetch_watchlist(wl_id, default_radius):
+    """
+    Fetch the watchlist from the database. If radius is not supplied per cone, 
+    then use the default radius of the watchlist
+    """
+    msl = connect_db()
+    cursor = msl.cursor(buffered=True, dictionary=True)
+
+    cursor.execute('SELECT cone_id, ra, decl, radius, name FROM watchlist_cones WHERE wl_id=%d ' % wl_id)
+    cone_ids = []
+    ralist   = []
+    delist   = []
+    radius   = []
+    names    = []
+    ncone = 0
+    # Build lists of all the data from the database
+    for row in cursor:
+        r = default_radius
+        if row['radius'] is not None: 
+            r = row['radius']
+        cone_ids.append(row['cone_id'])
+        ralist  .append(row['ra'])
+        delist  .append(row['decl'])
+        radius  .append(r/3600.0)
+        names   .append(row['name'])
+        ncone += 1
+#    print('wl_id=%d has %d cones' % (wl_id, ncone))
+    return {'cone_ids':cone_ids, 'ra':ralist, 'de':delist, 'radius':radius, 'names':names}
+
+def fetch_active_watchlists():
+    """
+    Go through the database and fetch the active watchlists
+    Select those fresher than their cache and rebuild their cache.
+    """
+    msl = connect_db()
+    cursor = msl.cursor(buffered=True, dictionary=True)
+
+    watchlist_list = []
+    cursor.execute('SELECT wl_id, name, radius, timestamp FROM watchlists WHERE active > 0 ')
+    for row in cursor:
+        # unix time of last update from the database
+        watchlist_timestamp = time.mktime(row['timestamp'].timetuple())
+
+        # directory where the cache files are kept
+        watchlist_dir = settings.WATCHLIST_MOCS + 'wl_%d'%row['wl_id']
+
+        try:
+            # unix time of last modification of this directory
+            dir_timestamp = os.stat(watchlist_dir)[ stat.ST_MTIME ] 
+        except:
+            dir_timestamp = 0
+        newer = watchlist_timestamp - dir_timestamp
+
+        # if the watchlist from the database is newer than the cache, rebuild it
+        # print(row['wl_id'], 'watchlist newer by %d seconds'%newer)
+        if newer > 0:
+            watchlist_list.append({'wl_id':row['wl_id'], 'name':row['name'],'radius':row['radius']})
+    # watchlists which will have their caches rebuilt
+    return watchlist_list
+
+def main():
+    max_depth = settings.WATCHLIST_MAX_DEPTH
+
+    # who needs to be recomputed
+    watchlists = fetch_active_watchlists()
+
+    for watchlist in watchlists:
+        # get the data from the database
+        cones = fetch_watchlist(watchlist['wl_id'], watchlist['radius'])
+
+        t = time.time()
+        # clear the cache and remake the directory
+        watchlist_dir = settings.WATCHLIST_MOCS + 'wl_%d/'%watchlist['wl_id']
+        try:    os.system('rm -r ' + watchlist_dir)
+        except: pass
+        os.mkdir(watchlist_dir)
+
+        # compute the list of mocs
+        moclist = moc_watchlists(cones, max_depth)
+        
+        # write the watchlist.csv
+        w = open(watchlist_dir + 'watchlist.csv', 'w')
+        ralist   = cones['ra']
+        delist   = cones['de']
+        radius   = cones['radius']
+        names    = cones['names']
+        cone_ids = cones['cone_ids']
+        for i in range(len(ralist)):
+            w.write('%d, %f, %f, %.3e, %s\n' % 
+                (cone_ids[i], ralist[i], delist[i], radius[i], names[i]))
+
+        # now write the moc files
+        for i in range(len(moclist)):
+            moclist[i].write(watchlist_dir + 'moc%03d.fits'%i)
+        print('Watchlist "%s" with %d cones rebuilt in %.2f seconds' 
+                % (watchlist['name'], len(ralist), time.time() - t))
+
+if __name__ == "__main__":
+    main()
