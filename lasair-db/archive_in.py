527970e1bf8396ecefb734c55156e82da66311b5
Roy Williams <roy@roe.ac.uk>
2020-10-08 14:00:15 +0000

    update

--- lasair-db/archive_in.py
+++ lasair-db/archive_in.py
@@ -1,10 +1,10 @@
 import os, sys
 import settings
 file = sys.argv[1]
-print(file)
 tok = file.split('__')
 node = tok[0]
 dbtable = tok[1]
+print(dbtable, ' from ', node)
 
 sql  = "LOAD DATA LOCAL INFILE '/home/ubuntu/scratch/%s' " % file
 sql += "REPLACE INTO TABLE %s FIELDS TERMINATED BY ',' " % dbtable
