66e215bba352907b5d918942ba8e7e869d2e1c26
G Francis <g.francis@ed.ac.uk>
2020-10-23 10:22:03 +0000

    implemented query

--- sherlocksvc/app/main.py
+++ sherlocksvc/app/main.py
@@ -6,6 +6,7 @@ from flask import Flask, request
 from flask_restful import Api, Resource, reqparse, inputs
 import json
 import yaml
+#import logging
 #import subprocess
 #import tempfile
 #from os import unlink
@@ -19,24 +20,21 @@ conf = {
         'sherlock_settings': 'sherlock.yaml'
         }
 
-def classify(name):
-    sherlock_settings = {}
+def classify(name,ra,dec,lite=False):
     with open(conf['sherlock_settings'], "r") as f:
         sherlock_settings = yaml.safe_load(f)
-    names = []
-    ra = []
-    dec = []
-    names.append(name)
-    classifier = transient_classifier(
-        settings=sherlock_settings,
-        ra=ra,
-        dec=dec,
-        name=names,
-        verbose=1,
-        updateNed=False
-    )
-    classifications, crossmatches = classifier.classify()
-    return classifications, crossmatches
+        classifier = transient_classifier(
+            log=app.logger,
+            settings=sherlock_settings,
+            ra=ra,
+            dec=dec,
+            name=name,
+            verbose=1,
+            updateNed=False,
+            lite=lite
+        )
+        classifications, crossmatches = classifier.classify()
+        return classifications, crossmatches
 
 class Object(Resource):
     """Get the Sherlock crossmatch results for a named object.
@@ -63,11 +61,29 @@ class Query(Resource):
     """Run Sherlock for a given position.
 
     Parameters:
+        name (string): Name of object. Optional.
         ra (float): right ascension. Required.
-        dec (float): declination. Required. """
+        dec (float): declination. Required.
+        lite (boolean): produce top ranked matches only. Default False."""
 
     def get(self):
-        return '', 501
+        parser = reqparse.RequestParser()
+        parser.add_argument("name", type=inputs.regex("^[\w\-]+$"), default="query")
+        parser.add_argument("ra", type=inputs.regex("^[0-9\.]*$"), required=True)
+        parser.add_argument("dec", type=inputs.regex("^[0-9\.]*$"), required=True)
+        parser.add_argument("lite", type=inputs.boolean, default=False)
+        message = request.data
+        args = parser.parse_args()
+        classifications, crossmatches = classify(
+                args['name'],
+                args['ra'],
+                args['dec'],
+                lite=args['lite'])
+        result = {
+                'classifications': classifications,
+                'crossmatches': crossmatches
+                }
+        return result, 200
     def post(self):
         return self.get()
 
