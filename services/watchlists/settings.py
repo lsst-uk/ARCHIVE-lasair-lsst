46d8c7fdc8dbb02b20c8c088b11ace5f958f7700
Roy Williams <roy@roe.ac.uk>
2020-12-16 15:38:49 +0000

    services

--- services/watchlists/settings.py
+++
@@ -1,9 +0,0 @@
-DB_HOST         = '192.168.0.12'
-DB_USER_READ    = 'readonly_ztf2'
-DB_PASS_READ    = 'OPV537car'
-
-TNS_WATCHLIST_ID    = 141
-
-WATCHLIST_MOCS      = '/mnt/cephfs/roy/watchlists'
-WATCHLIST_CHUNK     = 50000
-WATCHLIST_MAX_DEPTH = 13
