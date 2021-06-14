c84222dc2a2108e3fbddbbb462ea14fb1daf0ce2
Roy Williams <roy@roe.ac.uk>
2021-06-14 10:13:29 +0000

    update

--- utility/slack_webhook.py
+++ utility/slack_webhook.py
@@ -1,10 +1,9 @@
 import requests, json
-import settings
 
-def send(message):
+def send(url, message):
     data = {'channel': '#monitoring-lasair-ztf', 'text': message}
 
-    response = requests.post(settings.url, data=json.dumps(data),
+    response = requests.post(url, data=json.dumps(data),
         headers={'Content-Type': 'application/json'})
 
     if response.status_code != 200:
