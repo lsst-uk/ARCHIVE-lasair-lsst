0acf5c63c9eabf77a21417523bf2f77717a2eee9
G Francis <g.francis@epcc.ed.ac.uk>
2021-06-16 11:25:21 +0000

    add wrapper runner to provide slack alerts and auto restart

--- sherlock_wrapper/wrapper.py
+++ sherlock_wrapper/wrapper.py
@@ -5,7 +5,7 @@ adds the Sherlock classification and crossmatches back into the alert and
 republishes on the output topic.
 """
 
-__version__ = "0.5.10"
+__version__ = "0.5.13"
 
 import warnings
 import json
---
+++ sherlock_wrapper/wrapper_runner.py
@@ -0,0 +1,36 @@
+"""Run the Sherlock wrapper, monitor the output and send a slack alert on errors.
+Attempt to restart if the wrapper process exits, with an exponential backoff"""
+
+import subprocess
+import sys
+import re
+import time
+import slack_webhook
+sys.path.append('/opt/lasair/')
+import settings
+
+delay = 60
+max_delay = 21600
+sys.argv.pop(0)
+
+print ("Starting Sherlock wrapper.")
+sys.stdout.flush()
+
+while True:
+    proc = subprocess.Popen(sys.argv, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    while True:
+        rbin = proc.stdout.readline()
+        if len(rbin) == 0: break
+        line = rbin.decode('UTF-8').rstrip()
+        print (line)
+        sys.stdout.flush()
+        if re.search("(ERROR:)|(CRITICAL:)", line):
+            slack_webhook.send(settings.SLACK_URL, line)
+    proc.wait()
+    time.sleep(delay)
+    delay = delay * 2
+    if delay > max_delay:
+        delay = max_delay
+    print ("Attempting to restart Sherlock wrapper.")
+    slack_webhook.send(settings.SLACK_URL, "Attempting to restart Sherlock wrapper.")
+
