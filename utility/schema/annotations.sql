64bba3c15f50d2fd93c4504947f8e3ea488afe11
Roy Williams <roy@roe.ac.uk>
2021-09-25 08:37:19 +0000

    update

---
+++ utility/schema/annotations.sql
@@ -0,0 +1,14 @@
+USE ztf;
+CREATE TABLE IF NOT EXISTS annotations(
+`annotationID` int NOT NULL AUTO_INCREMENT,
+`objectId` varchar(16) NOT NULL,
+`topic` varchar(16) REFERENCES annotators(topic),
+`version` varchar(16) DEFAULT 0.1,
+`timestamp` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+`classification` varchar(16) NOT NULL,
+`explanation` text,
+`classdict` JSON,
+`url` text DEFAULT NULL,
+PRIMARY KEY    (annotationID),
+UNIQUE KEY     one_per_object (objectId, topic)
+)
