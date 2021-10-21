aa5c9bc50328a079a8d2f49fc5d71bcab8133463
Roy Williams <roy@roe.ac.uk>
2021-10-21 08:08:08 +0000

    modified manage_status

--- utility/manage_status.py
+++ utility/manage_status.py
@@ -1,9 +1,8 @@
 """
 manage_status.py
-Manage a status file that is a set of key-value pairs in JSON. 
+Manage a set of status files that is a set of key-value pairs in JSON. 
 Different processes/cores/threads can change key value or increment values.
-There is a "signal" key with the increment: when this is different from
-the signal value in the file, increments start at zero
+There is a "file_id" to determine which file we are working with
 """
 
 import datetime
@@ -15,113 +14,135 @@ SLEEPTIME = 0.1
 class manage_status():
     """ manage_status.
         Args:
-            signal_key: The special key, when its value changes, increment from zero
+            file_id: The special key, when its value changes, increment from zero
             status_file: Name of the status file
     """
-    def __init__(self, signal_key, status_file):
-        self.signal_key  = signal_key
-        self.status_file = status_file
-        self.lock_file   = status_file + '.lock'
-        if not os.path.exists(self.status_file) and not os.path.exists(self.lock_file):
-            print('Status file not present!')
-            f = open(self.status_file, 'w')
+    def __init__(self, status_file_root):
+        self.status_file_root  = status_file_root
+
+    def read(self, file_id):
+        status_file = '%s_%s.json' % (self.status_file_root, str(file_id))
+        f = open(status_file)
+        status = json.loads(f.read())
+        f.close()
+        return status
+
+    def lock_read(self, file_id):
+        """ lock_read.
+            If status file not present, make an empty one
+            Waits for lock, then locks and returns the status
+            Must be quickly followed with write_unlock!
+            Args:
+                file_id: which file to use
+        """
+        status_file = '%s_%s.json' % (self.status_file_root, str(file_id))
+        lock_file   = '%s_%s.lock' % (self.status_file_root, str(file_id))
+
+        if not os.path.exists(status_file) and not os.path.exists(lock_file):
+#            print('Status file not present!')
+            f = open(status_file, 'w')
             f.write('{}')
             f.close()
-
-    def lock_read(self):
-        """ lock_read.
-            Waits for lock, then locks and returns the status
-        """
+        # rename as lock file while we modify values
         while 1:
             try:
-                os.rename(self.status_file, self.lock_file)
+                os.rename(status_file, lock_file)
                 break
             except:
                 time.sleep(SLEEPTIME)
 
-#        print('locked   %d' % (time.time()%1000))  ##### hack hack
-        f = open(self.lock_file)
+        # return contents
+        f = open(lock_file)
         status = json.loads(f.read())
         f.close()
-#        time.sleep(1)   #### hack hack
         return status
 
-    def write_unlock(self, status):
+    def write_unlock(self, status, file_id):
         """ write_status:
             Writes the status file, then unlocks
             Args:
                 status: dictionary of key-value pairs
+                file_id: which file to use
         """
         update_time = datetime.datetime.utcnow().isoformat()
         update_time = update_time.split('.')[0]
         status['update_time'] = update_time
-        f = open(self.lock_file, 'w')
+
+        status_file = '%s_%s.json' % (self.status_file_root, str(file_id))
+        lock_file   = '%s_%s.lock' % (self.status_file_root, str(file_id))
+
+        # dump the status to the lock file
+        f = open(lock_file, 'w')
         f.write(json.dumps(status))
         f.close()
-        os.rename(self.lock_file, self.status_file)
-#        print('unlocked %d' % (time.time()%1000))  ##### hack hack
+        # rename lock file as atatus file
+        os.rename(lock_file, status_file)
 
-    def __repr__(self):
+    def tostr(self, file_id):
         """ __repr__:
             Write out the status file
         """
+        status_file = '%s_%s.json' % (self.status_file_root, str(file_id))
         try:
-            f = open(self.status_file)
+            f = open(status_file)
             status = json.loads(f.read())
             f.close()
         except:
             status = {}
         return json.dumps(status, indent=2)
 
-    def set(self, dictionary):
+    def set(self, dictionary, file_id):
         """ set.
             Puts the kv pairs from the dictionary into the status file
             Args:
                 dictionary: set of key-value pairs
+                file_id: which file to use
         """
-        status = self.lock_read()
-
-        if not self.signal_key in status:
-            status[self.signal_key] = 0
-
+        status = self.lock_read(file_id)
         for key,value in dictionary.items():
             status[key] = value
+        self.write_unlock(status, file_id)
 
-        self.write_unlock(status)
-
-    def add(self, dictionary, signal_value):
+    def add(self, dictionary, file_id):
         """ add
             Increments the given keys with the given values
             Args:
                 dictionary: set of key-value pairs
-                signal_value: if same as in status file, increment, else set
+                file_id: if same as in status file, increment, else set
         """
-        status = self.lock_read()
+        status = self.lock_read(file_id)
 
-        # if the signal is not present, increment from zero
-        if not self.signal_key in status:
-            status[self.signal_key] = signal_value
-            for key,value in dictionary.items():
-                status[key]  = value
+        for key,value in dictionary.items():
+            if key in status: status[key] += value
+            else:             status[key]  = value
 
-        # if the signal is present and the same as the argument, then increment if possible
-        elif status[self.signal_key] == signal_value:
-            for key,value in dictionary.items():
-                if key in status: status[key] += value
-                else:             status[key]  = value
-
-        # if the signal has changed, increment from zero
-        else:
-            status = {}
-            status[self.signal_key] = signal_value
-            for key,value in dictionary.items():
-                status[key]  = value
-        self.write_unlock(status)
+        self.write_unlock(status, file_id)
 
 if __name__ == '__main__':
-    for i in range(10):
-        ms = manage_status('nid', './status.json')
-        ms.set({'banana':5, 'orange':6})
-        ms.add({'apple':12, 'pear':7}, 6)
-        ms.add({'apple':12, 'pear':7}, 6)
-        time.sleep(0.7)
+    import os
+    os.system('mkdir play')
+
+    ms = manage_status('play/status')
+    ms.set({'banana':5, 'orange':6}, 6)
+    ms.add({'apple':12, 'pear':7}, 6)
+    ms.add({'apple':12, 'pear':1}, 6)
+
+    ms.add({'apple':1, 'pear':7}, 7)
+    ms.add({'apple':1, 'pear':7}, 7)
+    ms.set({'banana':5, 'orange':6}, 7)
+    ms.set({'banana':4, 'orange':7}, 7)
+    ms.set({'banana':3, 'orange':8}, 7)
+
+    status = ms.read(6)
+    assert(status['banana'] == 5)
+    assert(status['orange'] == 6)
+    assert(status['apple']  == 24)
+    assert(status['pear']   == 8)
+
+    status = ms.read(7)
+    assert(status['banana'] == 3)
+    assert(status['orange'] == 8)
+    assert(status['apple']  == 2)
+    assert(status['pear']  == 14)
+
+    os.system('rm -r play')
