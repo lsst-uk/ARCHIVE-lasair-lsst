eccccf199f93f7175e0709f109fa76e888956c5a
Roy Williams <roy@roe.ac.uk>
2021-10-21 08:17:47 +0000

    modified manage_status

--- tests/manage_status_test.py
+++ tests/manage_status_test.py
@@ -1,6 +1,6 @@
 import os, sys, json
 import unittest
-python_path = '../../utility/'
+python_path = '../utility/'
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), python_path)))
 from manage_status import manage_status
 
