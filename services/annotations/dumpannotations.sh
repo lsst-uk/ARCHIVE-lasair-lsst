97add53525aefa94610656c704fba6eee4bf4c87
Roy Williams <roy@roe.ac.uk>
2021-11-02 09:03:20 +0000

    update

--- services/annotations/dumpannotations.sh
+++ services/annotations/dumpannotations.sh
@@ -1,2 +1,2 @@
 ssh 192.168.0.12 'sudo sh /home/ubuntu/lasair-lsst/lasair-db/annotations_dump.sh'
-python3 countAnnotations.py
+python3 /home/ubuntu/lasair-lsst/services/annotations/countAnnotations.py
