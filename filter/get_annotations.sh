5f91f0209d078e318cdc59a1e0fe3772e1e20cde
Roy Williams <roy@roe.ac.uk>
2021-11-25 15:42:31 +0000

    update

--- filter/get_annotations.sh
+++ filter/get_annotations.sh
@@ -1,2 +1,4 @@
+logfile='/mnt/cephfs/lasair/services_log/'$(date +'%Y%m%d')'.log'
+echo 'Pull annotations to filter node at' $(date) >> $logfile
 mysql -u ztf -p ztf -p123password < /mnt/cephfs/lasair/annotations/annotations.sql
-exit 0
+mysql -u ztf -p123password ztf -e 'select count(*) from annotations' >> $logfile
--- filter/get_crossmatch_tns.sh
+++ filter/get_crossmatch_tns.sh
@@ -1,2 +1,4 @@
+logfile='/mnt/cephfs/lasair/services_log/'$(date +'%Y%m%d')'.log'
+echo 'Pull crossmatch_tns to filter node at' $(date) >> $logfile
 mysql -u ztf -p ztf -p123password < /mnt/cephfs/lasair/crossmatch_tns/crossmatch_tns.sql
-exit 0
+mysql -u ztf -p123password ztf -e 'select count(*) from crossmatch_tns' >> $logfile
