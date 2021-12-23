5279a4c0c10880d7da275d026c1e725752c14842
Roy Williams <roy@roe.ac.uk>
2021-12-23 09:57:49 +0000

    rearrange and document

--- filter/run_active_queries.py
+++ filter/run_active_queries.py
@@ -1,5 +1,35 @@
 """ Runs the user's queries. 
 Fetch them from database, construct SQL, execute, produce kafka
+
+(1) fetch_queries(): 
+gets all the queries from the database
+
+(2) run_annotation_queries(query_list): 
+may be called to get all the recent fast annotations 
+
+(3) run_queries(query_list, annotation_list=None):
+Uses query_list and possibly annotation_list and runs all the queries against 
+local, or those involving annotator against master databaase
+
+(4) query_for_object(query, objectId):
+If doing fast annotations, convert a given query with specific objectId
+
+(5) run_query(query, msl, annotator=None, objectId=None):
+Run a specific query and return query_results
+
+(6) dispose_query_results(query, query_results):
+Deal with the query results
+
+(6a) fetch_digest(topic_name):
+    Get the digest file from shared storage
+
+(6b) dispose_email(allrecords, last_email):
+    Deal with outgoing emails, it calls this to actually send
+    send_email(email, topic, message, message_html):
+
+(6c) dispose_kafka(query_results, topic):
+    Produce Kafka output to public stream
+
 """
 
 import os, sys
@@ -14,58 +44,89 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-def db_connect_remote():
-    config = {
-        'user'    : settings.DB_USER_REMOTE,
-        'password': settings.DB_PASS_REMOTE,
-        'host'    : settings.DB_HOST_REMOTE,
-        'port'    : settings.DB_PORT_REMOTE,
-        'database': 'ztf'
-    }
-    return mysql.connector.connect(**config)
-
-def db_connect_local():
-    config = {
-        'user'    : settings.DB_USER_LOCAL,
-        'password': settings.DB_PASS_LOCAL,
-        'host'    : settings.DB_HOST_LOCAL,
-        'database': 'ztf'
-    }
-    return mysql.connector.connect(**config)
-
-def send_email(email, topic, message, message_html):
-    """send_email.
-
-    Args:
-        email:
-        topic:
-        message:
+def fetch_queries():
+    """fetch_queries.
+    Get all the stored queries from the master database
     """
-    msg = MIMEMultipart('alternative')
+    # first get the user queries from the database that the webserver uses
+    msl_remote = db_connect_remote()
 
-    msg['Subject'] = 'Lasair query ' + topic
-    msg['From']    = 'donotreply@lasair.roe.ac.uk'
-    msg['To']      = email
+    # Fetch all the stored queries from the master database
+    cursor   = msl_remote.cursor(buffered=True, dictionary=True)
+    query = 'SELECT mq_id, user, name, email, tables, active, real_sql, topic_name '
+    query += 'FROM myqueries, auth_user WHERE myqueries.user = auth_user.id AND active > 0'
+    cursor.execute(query)
 
-    msg.attach(MIMEText(message, 'plain'))
-    msg.attach(MIMEText(message_html, 'html'))
-    s = smtplib.SMTP('localhost')
-    s.sendmail('donotreply@lasair.roe.ac.uk', email, msg.as_string())
-    s.quit()
+    query_list = []
+    for query in cursor:
+        query_dict = {
+            'mq_id':     query['mq_id'],
+            'user':      query['user'],
+            'name':      query['name'],
+            'active':    query['active'],
+            'email':     query['email'],
+            'tables':    query['tables'],
+            'real_sql':  query['real_sql'],
+            'topic_name':query['topic_name'],
+        }
+        query_list.append(query_dict)
+    return query_list
 
-def datetime_converter(o):
-    """datetime_converter.
-
-    Args:
-        o:
+def run_annotation_queries(query_list):
+    """run_annotation_queries.
+    Pulls the recent content from the kafka topic 'ztf_annotations' 
+    Each message has an annotator/topic name, and the objectId that was annotated.
+    Queries that have that annotator should run against that object
     """
-# used by json encoder when it gets a type it doesn't understand
-    if isinstance(o, datetime.datetime):
-        return o.__str__()
+    annotation_list = []
+    conf = {
+        'bootstrap.servers':   settings.KAFKA_HOST,
+        'group.id':            settings.ANNOTATION_GROUP_ID,
+        'default.topic.config': {'auto.offset.reset': 'smallest'}
+    }
+    streamReader = Consumer(conf)
+    topic = 'ztf_annotations'
+    streamReader.subscribe([topic])
+    while 1:
+        msg = streamReader.poll(timeout=5)
+        if msg == None: break
+        ann = json.loads(msg.value())
+        annotation_list.append(ann)
+    streamReader.close()
+    #print('got ', annotation_list)
+    run_queries(query_list, annotation_list)
 
-def kafka_ack(err, msg):
-    if err is not None:
-        print("Failed to deliver message: %s: %s" % (str(msg), str(err)))
+def run_queries(query_list, annotation_list=None):
+    """
+    When annotation_list is None, it runs all the queries against the local database
+    When not None, runs some queires agains a specific object, using the master database
+    """
+    try:
+        msl_local = db_connect_local()
+    except:
+        print('ERROR in filter/run_active_queries: cannot connect to local database')
+        sys.stdout.flush()
+
+    for query in query_list:
+        n = 0
+        t = time.time()
+
+        # normal case of streaming queries
+        if not annotation_list:  
+            query_results = run_query(query, msl_local)
+            n += dispose_query_results(query, query_results)
+
+        # immediate response to active=2 annotators
+        else:
+            for ann in annotation_list:  
+                msl_remote = db_connect_remote()
+                query_results = run_query(query, msl_remote, ann['annotator'], ann['objectId'])
+                n += dispose_query_results(query, query_results)
+
+        t = time.time() - t
+        if n > 0:
+            print('   %s got %d in %.1f seconds' % (query['topic_name'], n, t))
+            sys.stdout.flush()
 
 def query_for_object(query, objectId):
     """ modifies an existing query to add a new constraint for a specific object.
@@ -131,6 +192,41 @@ def run_query(query, msl, annotator=None, objectId=None):
 
     return query_results
 
+def dispose_query_results(query, query_results):
+    """ Send out the query results by email or kafka, and ipdate the digest file
+    """
+    if len(query_results) == 0:
+        return 0
+    active = query['active']
+    digest,last_entry,last_email = fetch_digest(query['topic_name'])
+    utcnow = datetime.datetime.utcnow()
+    allrecords = (query_results + digest)[:10000]
+
+    if active == 1:
+        # send results by email if 24 hurs has passed, returns time of last email send
+        last_email = dispose_email(allrecords, last_email)
+
+    if active == 2:
+        # send results by kafka on given topic
+        dispose_kafka(query_results, query['topic_name'])
+
+    # update the digest file
+    utcnow_text = utcnow.strftime("%Y-%m-%d %H:%M:%S")
+    last_email_text = last_email.strftime("%Y-%m-%d %H:%M:%S")
+    digest_dict = {
+            'last_entry': utcnow_text, 
+            'last_email':last_email_text, 
+            'digest':allrecords
+            }
+    digestdict_text = json.dumps(digest_dict, indent=2, default=datetime_converter)
+
+    filename = settings.KAFKA_STREAMS + query['topic_name']
+    f = open(filename, 'w')
+    os.chmod(filename, 0O666)
+    f.write(digestdict_text)
+    f.close()
+    return len(query_results)
+
 def fetch_digest(topic_name):
     filename = settings.KAFKA_STREAMS + topic_name
     try:
@@ -183,6 +279,26 @@ def dispose_email(allrecords, last_email):
         sys.stdout.flush()
         return last_email
 
+def send_email(email, topic, message, message_html):
+    """send_email.
+
+    Args:
+        email:
+        topic:
+        message:
+    """
+    msg = MIMEMultipart('alternative')
+
+    msg['Subject'] = 'Lasair query ' + topic
+    msg['From']    = 'donotreply@lasair.roe.ac.uk'
+    msg['To']      = email
+
+    msg.attach(MIMEText(message, 'plain'))
+    msg.attach(MIMEText(message_html, 'html'))
+    s = smtplib.SMTP('localhost')
+    s.sendmail('donotreply@lasair.roe.ac.uk', email, msg.as_string())
+    s.quit()
+
 def dispose_kafka(query_results, topic):
     """ Send out query results by kafka to the given topic.
     """
@@ -207,124 +323,38 @@ def dispose_kafka(query_results, topic):
         print(rtxt)
         sys.stdout.flush()
 
-def dispose_query_results(query, query_results):
-    """ Send out the query results by email or kafka, and ipdate the digest file
-    """
-    if len(query_results) == 0:
-        return 0
-    active = query['active']
-    digest,last_entry,last_email = fetch_digest(query['topic_name'])
-    utcnow = datetime.datetime.utcnow()
-    allrecords = (query_results + digest)[:10000]
-
-    if active == 1:
-        # send results by email
-        last_email = dispose_email(allrecords, last_email)
-
-    if active == 2:
-        # send results by kafka
-        dispose_kafka(query_results, query['topic_name'])
-
-    # update the digest file
-    utcnow_text = utcnow.strftime("%Y-%m-%d %H:%M:%S")
-    last_email_text = last_email.strftime("%Y-%m-%d %H:%M:%S")
-    digest_dict = {
-            'last_entry': utcnow_text, 
-            'last_email':last_email_text, 
-            'digest':allrecords
-            }
-    digestdict_text = json.dumps(digest_dict, indent=2, default=datetime_converter)
-
-    filename = settings.KAFKA_STREAMS + query['topic_name']
-    f = open(filename, 'w')
-    os.chmod(filename, 0O666)
-    f.write(digestdict_text)
-    f.close()
-    return len(query_results)
-
-def fetch_queries():
-    """fetch_queries.
-    Get all the stored queries from the master database
-    """
-    # first get the user queries from the database that the webserver uses
-    msl_remote = db_connect_remote()
-
-    # Fetch all the stored queries from the master database
-    cursor   = msl_remote.cursor(buffered=True, dictionary=True)
-    query = 'SELECT mq_id, user, name, email, tables, active, real_sql, topic_name '
-    query += 'FROM myqueries, auth_user WHERE myqueries.user = auth_user.id AND active > 0'
-    cursor.execute(query)
-
-    query_list = []
-    for query in cursor:
-        query_dict = {
-            'mq_id':     query['mq_id'],
-            'user':      query['user'],
-            'name':      query['name'],
-            'active':    query['active'],
-            'email':     query['email'],
-            'tables':    query['tables'],
-            'real_sql':  query['real_sql'],
-            'topic_name':query['topic_name'],
-        }
-        query_list.append(query_dict)
-    return query_list
-
-def run_queries(query_list, annotation_list=None):
-    """
-    When annotation_list is None, it runs all the queries against the local database
-    When not None, runs some queires agains a specific object, using the master database
-    """
-    try:
-        msl_local = db_connect_local()
-    except:
-        print('ERROR in filter/run_active_queries: cannot connect to local database')
-        sys.stdout.flush()
-
-    for query in query_list:
-        n = 0
-        t = time.time()
-
-        # normal case of streaming queries
-        if not annotation_list:  
-            query_results = run_query(query, msl_local)
-            n += dispose_query_results(query, query_results)
-
-        # immediate response to active=2 annotators
-        else:
-            for ann in annotation_list:  
-                msl_remote = db_connect_remote()
-                query_results = run_query(query, msl_remote, ann['annotator'], ann['objectId'])
-                n += dispose_query_results(query, query_results)
-
-        t = time.time() - t
-        if n > 0:
-            print('   %s got %d in %.1f seconds' % (query['topic_name'], n, t))
-            sys.stdout.flush()
-
-def run_annotation_queries(query_list):
-    """run_annotation_queries.
-    Pulls the recent content from the kafka topic 'ztf_annotations' 
-    Each message has an annotator/topic name, and the objectId that was annotated.
-    Queries that have that annotator should run against that object
-    """
-    annotation_list = []
-    conf = {
-        'bootstrap.servers':   settings.KAFKA_HOST,
-        'group.id':            settings.ANNOTATION_GROUP_ID,
-        'default.topic.config': {'auto.offset.reset': 'smallest'}
+def db_connect_remote():
+    config = {
+        'user'    : settings.DB_USER_REMOTE,
+        'password': settings.DB_PASS_REMOTE,
+        'host'    : settings.DB_HOST_REMOTE,
+        'port'    : settings.DB_PORT_REMOTE,
+        'database': 'ztf'
     }
-    streamReader = Consumer(conf)
-    topic = 'ztf_annotations'
-    streamReader.subscribe([topic])
-    while 1:
-        msg = streamReader.poll(timeout=5)
-        if msg == None: break
-        ann = json.loads(msg.value())
-        annotation_list.append(ann)
-    streamReader.close()
-    #print('got ', annotation_list)
-    run_queries(query_list, annotation_list)
+    return mysql.connector.connect(**config)
+
+def db_connect_local():
+    config = {
+        'user'    : settings.DB_USER_LOCAL,
+        'password': settings.DB_PASS_LOCAL,
+        'host'    : settings.DB_HOST_LOCAL,
+        'database': 'ztf'
+    }
+    return mysql.connector.connect(**config)
+
+def datetime_converter(o):
+    """datetime_converter.
+
+    Args:
+        o:
+    """
+# used by json encoder when it gets a type it doesn't understand
+    if isinstance(o, datetime.datetime):
+        return o.__str__()
+
+def kafka_ack(err, msg):
+    if err is not None:
+        print("Failed to deliver message: %s: %s" % (str(msg), str(err)))
 
 if __name__ == "__main__":
     sys.path.append('../utility/')
